700af76dd8744f762ead5118658e6aa5d081395c
Q <CharlieFresa@ya.ru>
2024-06-25 22:30:28 +0300

    configurated main.py and now is able to poll

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
+import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage, Redis
+import logging
+from environs import Env
+from handlers.user_handler import USER
 
 
 async def main():
+    env = Env()
+    env.read_env(path='.env')
+    bot = Bot(token=env('TOKEN'))
+    redis = Redis()
+    storage = RedisStorage(redis=redis)
+    dp = Dispatcher(storage=storage)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s'
+    )
+
+    logging.info('Starting bot')
+
+    # dp.update.outer_middleware(...)
+
+    logging.info('Connecting to the routers..')
+
+    dp.include_router(USER)
+
+
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
